12fb0f940fa00da8870af5cbda12da4f4a7c970e
nesrinaaa <bginny97@gmail.com>
2026-02-27 19:28:56 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from scrape import scrape_website
 st.title("web scraper app")
 url = st.text_input("Enter the URL of the website you want to scrape:")
 if st.button("Scrape site"):
     st.write(f"Scraping {url}...")
+    result = scrape_website(url)
+    print(result)
